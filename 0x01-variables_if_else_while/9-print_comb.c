aa2c999f83ef14cc517ec540b65c3ae144410b1f
abdelrhman892 <abdoayman5m@gmail.com>
2023-09-11 16:43:09 -0700

    new while statment

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: print 0,1, -9
+ *		while using int variable
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		putchar(digit + 48);
+
+		if (digit != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+		digit++;
+	}
+	putchar('\n');
+
+	return (0);
+}
