2eae5fb62b37d91399e4774bf693df680acac792
abdelrhman892 <abdoayman5m@gmail.com>
2023-09-11 16:36:36 -0700

    new while statment

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: print numbers of base16 in lowercase
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int digit = 48;
+
+	while (digit <= 102)
+	{
+		putchar(digit);
+
+		if (digit == 57)
+			digit += 39;
+		digit++;
+	}
+	putchar('\n');
+
+	return (0);
+}
