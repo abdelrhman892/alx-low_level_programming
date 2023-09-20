2db01d5ea28368a295bb90f5e1c8c8ead2e18697
abdelrhman892 <abdoayman5m@gmail.com>
2023-09-20 08:40:07 -0700

    sss

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -1,15 +1,19 @@
-#include <unistd.h>
-#include "main.h"
+#ifndef MAIN_H
+#define MAin_H
 
-/**
- * _putchar - writes the character c to stdout
- *
- * @c : the character to print
- *
- * Return: On success 1,
- *		On error, -1 is returned, and errmo is ser appropriately
-*/
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+#include <stdio.h>
+
+
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+
+#endif
