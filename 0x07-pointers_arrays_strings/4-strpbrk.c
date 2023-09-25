c12b6b492ac7151851aefe0b5f440c150aa1ad78
abdelrhman892 <abdoayman5m@gmail.com>
2023-09-25 04:59:35 -0700

    new

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -14,20 +14,19 @@ char *_strpbrk(char *s, char *accept)
 	char *p;
 
 	itr = 0;
-
 	while (s[itr] != '\0')
 	{
 		jtr = 0;
 		while (accept[jtr] != '\0')
 		{
-			if (accept[jtr] != '\0')
+			if (accept[jtr] == s[itr])
 			{
 				p = &s[itr];
 				return (p);
 			}
 			jtr++;
 		}
-		itr++
+		itr++;
 	}
 	return (0);
 }
