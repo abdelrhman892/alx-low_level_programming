a5b182d20f6b2918731b7ea429b70ade7cb4f69a
abdelrhman892 <abdoayman5m@gmail.com>
2023-10-08 13:22:27 -0700

    new

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * *_memset - memset
+ * @s: pointer
+ * @b: constant
+ * @n: max bytes to use
+ * Return: s
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *ptr = s;
+
+	while (n--)
+		*s++ = b;
+
+	return (ptr);
+}
+
+/**
+ * *_calloc - calloc
+ * @nmemb: array length
+ * @size: size
+ * Return: pointer
+*/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *m;
+
+	if (size == 0 || nmemb == 0)
+		return (NULL);
+	m = malloc(sizeof(int) * nmemb);
+
+	if (m == 0)
+		return (NULL);
+
+	_memset(m, 0, sizeof(int) * nmemb);
+
+	return (m);
+}
