095b860e432819fcb33c8fbb721c327b77bf4faf
abdelrhman <115238439+abdelrhman892@users.noreply.github.com>
2023-10-11 22:46:08 +0300

    ssss

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,67 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds
+ * @a: the first
+ * @b: the second
+ *
+ * Return: the sum
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subs
+ * @a: the first
+ * @b: the second
+ *
+ * Return: the sum
+*/
+
+int op_sub(int a, int b)
+{
+        return (a - b);
+}
+
+/**
+ * op_mul - muls
+ * @a: the first
+ * @b: the second
+ *
+ * Return: the sum
+*/
+
+int op_mul(int a, int b)
+{
+        return (a * b);
+}
+
+/**
+ * op_div - divs
+ * @a: the first
+ * @b: the second
+ *
+ * Return: the sum
+*/
+
+int op_div(int a, int b)
+{
+        return (a / b);
+}
+
+/**
+ * op_mod - mods
+ * @a: the first
+ * @b: the second
+ *
+ * Return: the sum
+*/
+
+int op_mod(int a, int b)
+{
+        return (a % b);
+}
+
