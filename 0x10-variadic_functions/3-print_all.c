3a058ecb961b697c223ee87f1ba7b192c03a18aa
abdelrhman <115238439+abdelrhman892@users.noreply.github.com>
2023-10-12 17:54:50 +0300

    To be is a to be the value of a variable

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -55,7 +55,7 @@ void print_all(const char * const format, ...)
 	char *separator = "";
 	va_list ap;
 	token_t tokens[] = {
-		{"c",format_char},
+		{"c", format_char},
 		{"i", format_int},
 		{"f", format_float},
 		{"s", format_string},
