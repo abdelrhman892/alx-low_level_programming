e19a61fc3ff9b2b08e6a1985ce8b877737d78db7
abdelrhman <115238439+abdelrhman892@users.noreply.github.com>
2023-10-12 18:25:37 +0300

    To be is a to be the value of a variable

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -42,9 +42,11 @@ void format_float(char *separator, va_list ap)
 void format_string(char *separator, va_list ap)
 {
 	char *str = va_arg(ap, char *);
-	switch((int)(!str))
-	case 1:
-		str = "(nil)";
+	
+	switch ((int)(!str))
+		case 1:
+			str = "(nil)";
+	
 	printf("%s%s", separator, str);
 }
 
