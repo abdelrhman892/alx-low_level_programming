460eebc644c4d346381b74ad0fd9aba46fa455a6
abdelrhman892 <abdoayman5m@gmail.com>
2023-10-27 10:40:28 -0700

    some tasks

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * get_bit - gets the bit
+ *
+ * @n: the number to index
+ * @index: the bit
+ *
+ * Return: the bit
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= sizeof(n) * 8)
+		return (-1);
+	return (n >> index & 1);
+}
---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * set_bit - sets the bit
+ *
+ * @n: the number
+ * @index: the bit
+ *
+ * Return: 1 if succes, -1 if felid
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(n) * 8)
+		return (-1);
+	return (!!(*n = 1L << index));
+}
