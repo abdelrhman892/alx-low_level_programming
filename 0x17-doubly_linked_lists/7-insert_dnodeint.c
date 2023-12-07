c01da4a93c13dedc9ef78eb5d05927dd4c614a10
abdelrhman892 <abdoayman@gmail.com>
2023-12-07 12:27:54 -0800

    ss

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -6,14 +6,14 @@
  * @idx: The position to insert the new node.
  * @n: The integer for the new node to contain.
  * Return: If the function fails NULL.
- *	otherwise the address of the new node.
-*/
+ *	otherwise the address of the new nod
+ */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *tmp = *h, *new;
 
 	if (idx == 0)
-		return (add_dnodeint (h, n));
+		return (add_dnodeint(h, n));
 	for (; idx != 1; idx--)
 	{
 		tmp = tmp->next;
@@ -22,7 +22,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 
 	if (tmp->next == NULL)
-		return (add_dnodeint_end (h, n));
+		return (add_dnodeint_end(h, n));
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
