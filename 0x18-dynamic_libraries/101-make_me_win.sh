329b467a5ccb7530baf2472550bcde0df2c70673
abdelrhman892 <abdoayman@gmail.com>
2023-12-11 07:29:25 -0800

    Code injection: Win the Giga Millions!

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https:/abdelrhman892/github.com/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
+wget -P /tmp https://github.com/abdelrhman892/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
 export LD_PRELOAD=/tmp/libgiga.so
