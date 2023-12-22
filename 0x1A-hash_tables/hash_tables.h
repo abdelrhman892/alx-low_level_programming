a7bbcef12e5efdcbae7af9eef7fa837d7da097ee
abdelrhman892 <abdoayman5m@gmail.com>
2023-12-22 12:23:11 -0800

    0. >>> ht = {}

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,3 +1,6 @@
+#ifndef _HASH_TABLES_H
+#define _HASH_TABLES_H
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -77,4 +80,5 @@ char *shash_table_get (const shash_table_t *ht, const char *key);
 void shash_table_print (const shash_table_t *ht);
 void shash_table_print_rev (const shash_table_t *ht);
 void shash_table_delete (shash_table_t *ht);
-#endif
+
+#endif /* _HASH_TABLES_H_ */
