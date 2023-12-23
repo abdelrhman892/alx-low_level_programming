d36ba9c9cbceebed78cdcb1b553e870697ebf517
abdelrhman892 <abdoayman5m@gmail.com>
2023-12-22 16:11:39 -0800

    ss

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -6,7 +6,7 @@
 #include <string.h>
 
 /**
-struct hash_node_s - Node of a hash table
+* struct hash_node_s - Node of a hash table
 * @key: The key, string
 * The key is unique in the HashTable
 * @value: The value corresponding to a key
@@ -14,22 +14,23 @@ struct hash_node_s - Node of a hash table
 */
 typedef struct hash_node_s
 {
-    char *key;
-    char *value;
-    struct hash_node_s *next;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
 } hash_node_t;
 
 /**
-* struct hash_table_s Hash table data structure
+* struct hash_table_s - Hash table data structure
 * @size: The size of the array
 * @array: An array of size @size
 * because we want our HashTable to use a Chaining collision handling
 **/
 typedef struct hash_table_s
 {
-    unsigned long int size;
-    hash_node_t **array;
+	unsigned long int size;
+	hash_node_t **array;
 } hash_table_t;
+
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
