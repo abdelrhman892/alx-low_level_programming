421295ee5598648eb996a1fe89d82954f3ad0093
abdelrhman892 <abdoayman5m@gmail.com>
2023-12-22 16:15:29 -0800

    ss

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,6 +1,6 @@
 #include "hash_tables.h"
 /**
-* key_index gives the index of a key
+* key_index - gives the index of a key
 * @key: key to get index for
 * @size: size of the hash table
 * Return: index for the key
